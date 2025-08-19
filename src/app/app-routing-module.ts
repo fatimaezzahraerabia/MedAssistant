1b84e09bd797e2f051c95b751e19a82b6007742e
imane <laarajimane22@gmail.com>
2025-08-19 12:46:05 +0100

    add authentication

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home.component';
 import { SearchPageComponent } from './pages/search/search.component';
+import { LoginComponent } from './auth/login/login';
+import { RegisterComponent } from './auth/register/register';
+
 
 export const routes: Routes = [ // Exporting routes
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent },
+  { path: '', component: HomePageComponent },
   { path: 'search', component: SearchPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+
+  
+
   // Add other routes here if neededC:\Users\Dell\Desktop\medfinder-app\src\app\app-routing-module.ts
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
 export class AppRoutingModule { }
